1d16e19c69bd7dcb18d1a5f5bfc8e43af15f149f
Rajat Mittal <rjmittal07@gmail.com>
2019-08-28 10:08:23 +0530

    minor changes

---
+++ Abs Banking/NetworkHelper.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkHelper.swift
+//  Abs Banking
+//
+//  Created by apple on 28/08/19.
+//  Copyright © 2019 Rajat. All rights reserved.
+//
+
+import SystemConfiguration
+/// Class helps to code reuse in handling internet network connections.
+class NetworkHelper {
+    /**
+     Verify if the device is connected to internet network.
+     - returns:          true if is connected to any internet network, false if is not
+     connected to any internet network.
+     */
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+            return false
+        }
+        
+        /* Only Working for WIFI
+         let isReachable = flags == .reachable
+         let needsConnection = flags == .connectionRequired
+         
+         return isReachable && !needsConnection
+         */
+        
+        // Working for Cellular and WIFI
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let ret = (isReachable && !needsConnection)
+        
+        return ret
+    } }
---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by apple on 28/08/19.
+//
+
+import Foundation
